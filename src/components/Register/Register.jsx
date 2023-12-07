2a04e6e60e86386ba8ac552d89fb6c4800e335a7
David Yepes <yepescastellanos@gmail.com>
2023-12-07 12:13:01 -0500

    actualizacionYepes

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './Register.css';
 
 export default function Register() {
   return (
-    <div>
-        <h1>hola</h1>
-    </div>
-  )
+    <section className="registro-container">
+      <form className="registro-form" onSubmit={(e) => e.preventDefault()}>
+        <h2>Registro</h2>
+        <label>
+          Usuario:
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Correo electrónico:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Número telefónico:
+          <input type="tel" name="phone" />
+        </label>
+        <label>
+          Contraseña:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Registrar</button>
+      </form>
+    </section>
+  );
 }
