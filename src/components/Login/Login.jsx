6607e199a7603f8076efa0d60e747f3c6d08c6de
Jhonw1 <bftpgjhonwilmar@gmail.com>
2024-02-11 23:27:40 -0500

    actualizado

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,35 +1,76 @@
-// Login.jsx
-
 import React, { useState } from 'react';
 import './Login.css';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
-    username: '',
+    email: '',
     password: '',
   });
+
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setCredentials({ ...credentials, [name]: value });
   };
 
+  const handleRedirect = () => {
+    // Redirigir al usuario a la página de inicio
+    window.location.href = '/';
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aquí puedes manejar la lógica de autenticación, por ejemplo, enviar credenciales a un servidor.
-    console.log('Credenciales enviadas:', credentials);
+
+    // Realizar la solicitud al backend
+    fetch('http://localhost:3000/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Manejar la respuesta del servidor
+        console.log('Respuesta del servidor:', data);
+
+        // Verificar si la autenticación fue exitosa
+        if (data.status === 'Successful authentication') {
+          // Almacenar el token en el estado o en localStorage para su uso posterior
+          const token = data.token;
+          // Puedes almacenar el token en el estado o en localStorage según tus necesidades
+          console.log('Token recibido:', token);
+          localStorage.setItem('authToken', token);
+
+          // Configurar el mensaje de éxito
+          setMessage('Inicio de sesión exitoso. ¡Bienvenido!');
+
+          // Redirigir al usuario a la página de inicio
+          handleRedirect();
+        } else {
+          // Manejar la autenticación fallida, mostrar un mensaje de error, etc.
+          console.error('Error de autenticación:', data.error);
+
+          // Configurar el mensaje de error
+          setMessage('Error de autenticación. Por favor, verifica tus credenciales.');
+        }
+      })
+      .catch(error => {
+        console.error('Error al enviar la solicitud:', error);
+      });
   };
 
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleSubmit}>
         <h2 className='iniciar'>Iniciar Sesión</h2>
-        <label htmlFor="username">Nombre de Usuario:</label>
+        <label htmlFor="email">Correo Electrónico:</label>
         <input
-          type="text"
-          id="username"
-          name="username"
-          value={credentials.username}
+          type="email"
+          id="email"
+          name="email"
+          value={credentials.email}
           onChange={handleChange}
           required
         />
@@ -45,6 +86,9 @@ const Login = () => {
         />
 
         <button type="submit">Iniciar Sesión</button>
+
+        {/* Mostrar el mensaje */}
+        <p className={message.includes('exitoso') ? 'success-message' : 'error-message'}>{message}</p>
       </form>
     </div>
   );
